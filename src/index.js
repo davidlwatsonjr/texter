e9dae8d78f95433c161102288b2e3e833dcedcea
David Watson <davidlwatsonjr@users.noreply.github.com>
2023-10-22 17:08:33 -0400

    Implement more GCP-specific logging levels

--- src/index.js
+++ src/index.js
@@ -11,12 +11,19 @@ app.use((req, res, next) => {
   next();
 });
 
-// Update error logger for platform-specifity if applicable
+// Update loggers for platform-specifity if applicable
 app.use((req, res, next) => {
   const {headers} = req;
 
-  // If the request is being handled by a Google Cloud Run service, update the console.error function
+  // If the request is being handled by a Google Cloud Run service
+  // Update the appropriate console functions
   if (headers['x-cloud-trace-context']) {
+    console._info = console.info;
+    console.info = (message) => console._info(JSON.stringify({severity: 'INFO', message}));
+
+    console._warn = console.warn;
+    console.warn = (message) => console._warn(JSON.stringify({severity: 'WARNING', message}));
+
     console._error = console.error;
     console.error = (message) => console._error(JSON.stringify({severity: 'ERROR', message}));
   }

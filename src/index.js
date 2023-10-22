37fdc6cc36408773fc1ef0deca05b7a41b447a63
David Watson <davidlwatsonjr@users.noreply.github.com>
2023-10-22 14:05:05 -0400

    Update error logging specifically for GCP

--- src/index.js
+++ src/index.js
@@ -11,6 +11,19 @@ app.use((req, res, next) => {
   next();
 });
 
+// Update error logger for platform-specifity if applicable
+app.use((req, res, next) => {
+  const {headers} = req;
+
+  // If the request is being handled by a Google Cloud Run service, update the console.error function
+  if (headers['x-cloud-trace-context']) {
+    console._error = console.error;
+    console.error = (message) => console._error(JSON.stringify({severity: 'ERROR', message}));
+  }
+
+  next();
+});
+
 // Authenticate request
 const { API_KEY } = process.env;
 app.use((req, res, next) => {

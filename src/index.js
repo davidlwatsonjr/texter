3ddb5d541a9e1ee3b3976b3adc63efcfd5227aa0
David Watson <davidlwatsonjr@users.noreply.github.com>
2024-09-02 12:40:30 -0400

    Use new SECRETS env var and API_KEY prefix in middleware

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const { SECRETS } = process.env;
+if (SECRETS) {
+  try {
+    const secrets = JSON.parse(SECRETS.replace(/\n/g, ""));
+    Object.keys(secrets).forEach((key) => {
+      process.env[key] = secrets[key];
+    });
+  } catch (e) {
+    console.error("Error parsing SECRETS JSON", e);
+  }
+}
+
 const express = require("express");
 
 const {
@@ -17,7 +29,7 @@ app.get("/ping", async (req, res) => {
   res.send("pong");
 });
 
-app.use(authAPIRequest);
+app.use(authAPIRequest("TEXTER"));
 
 app.get("/twilio/send", sendToTwilio);
 app.get("/send", sendToTwilio);
